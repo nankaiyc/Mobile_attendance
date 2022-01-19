4ce71b2251442f3d5fea183c3de24d94d0ba2d6a
wp wang <273257881@qq.com>
2022-01-19 17:29:00 +0800

    menu added

---
+++ miniprogram/components/menu/menu.js
@@ -0,0 +1,123 @@
+// components/menu/menu.js
+Component({
+  properties: {
+    // 这里定义了innerText属性，属性值可以在组件使用时指定
+    curIndex: {
+      type: String,
+      value: '0',
+    },
+    show: {
+      type: Boolean,
+      value: false,
+    },
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    itemArray: [
+      {
+        name: "考勤打卡",
+        url: "../../pages/checkIn/checkIn"
+      },
+      {
+        name: "考勤监管",
+        url: "../../pages/superVise/superVise"
+      },
+      {
+        name: "成员",
+        url: "../../pages/member/member"
+      },
+      {
+        name: "split",
+        url: ""
+      },
+      {
+        name: "我的",
+        url: "../../pages/person/person"
+      },
+      {
+        name: "设置",
+        url: "../../pages/setting/setting"
+      },
+    ]
+  },
+
+  methods: {
+    close_overlay() {
+    this.setData({
+      show: false
+    })
+  },
+  pop_left() {
+    this.setData({
+      show: true
+    })
+  },
+  jump(e) {
+    var index = e.target.dataset.index;
+    if (index == parseInt(this.properties.curIndex) || index == 3) {
+      return
+    }
+    wx.redirectTo({
+      url: this.data.itemArray[index].url,
+    })
+  }
+},
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
