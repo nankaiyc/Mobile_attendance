5059ff832200ef8a78e5f02129e721c2dd1e1fcb
wp wang <273257881@qq.com>
2022-02-18 21:35:23 +0800

    crypto added with aes base64 and md5

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,4 +1,5 @@
 //app.js
+const CryptoJS = require('./utils/crypto.js')
 App({
   onLaunch: function () {
     this.globalData = {}
@@ -12,5 +13,35 @@ App({
     }})
     let index = wx.getStorageSync('firstPageIndex');
     this.globalData.firstPage = index?index:0;
+
+    // this.crypto_example()
+  },
+
+  crypto_example() {
+    
+    let msg = {
+      'CLID': '08E4049FF24E4DD8C0E345398ACFB9EE',
+      '_s': '08E4049FF24E4DD8C0E345398ACFB9EE1398902400',
+      'OS': 'iOS'
+    }
+    msg = JSON.stringify(msg)
+    console.log('msg', msg)
+    let key = 'FE7A45426AFF5D14E52897E134F5CC33'
+    console.log('key', key)
+    const aes_msg = CryptoJS.AesEncrypt(msg, key)
+    console.log('aes 加密', aes_msg)
+    const base64_aes_msg = CryptoJS.Base64Encode(aes_msg)
+    console.log('base64 加密', base64_aes_msg)
+    const url_base64_aes_msg = encodeURIComponent(base64_aes_msg)
+    console.log('url 编码', url_base64_aes_msg)
+    
+    const base64_aes_msg_d = decodeURIComponent(url_base64_aes_msg)
+    console.log('url 解码', base64_aes_msg_d)
+    const aes_msg_d = CryptoJS.Base64Decode(base64_aes_msg_d)
+    console.log('base64 解密', aes_msg_d)
+    const msg_d = CryptoJS.AesDecrypt(aes_msg_d, key)
+    console.log('aes 解密', msg_d)
+
+    console.log(CryptoJS.Md5(key))
   }
 })
